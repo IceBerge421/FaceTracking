71ed2d1946ecf79776d693f4c00ba07b81534b9b
IceBerge421 <peterberge42@gmail.com>
2020-09-13 15:34:31 -0400

    Update demo_gltf.js

--- demos/threejs/gltf_fullScreen/demo_gltf.js
+++ demos/threejs/gltf_fullScreen/demo_gltf.js
@@ -4,7 +4,7 @@
 const SETTINGS = {
   gltfModelURL: 'https://raw.githubusercontent.com/IceBerge421/FaceTracking/master/demos/threejs/gltf_fullScreen/MASKOFF.gltf',
   cubeMapURL: 'Bridge2/',
-  offsetYZ: [0, 0], // offset of the model in 3D along vertical and depth axis
+  offsetYZ: [-0.5, 0], // offset of the model in 3D along vertical and depth axis
   scale: 1.3
 };
 
